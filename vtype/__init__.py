7ba76ef441f9688c19c8e5497b499b82d0d0f8ce
Parikshit Iyengar <2667254+piyengar@users.noreply.github.com>
2021-07-12 13:10:32 -0400

    added dataset type enum

--- vtype/__init__.py
+++ vtype/__init__.py
@@ -1,3 +1,4 @@
+from .dataset_type import DatasetType
 from .datamodule import TypeDataModule
 # from .model import ColorModel, valid_archs
 # from .prediction_writer import ColorPredictionWriter
---
+++ vtype/dataset_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class DatasetType(Enum):
+    VRIC= "VRIC"
+    CARS196 = "Cars196"
+    VEHICLE_ID ="VehicleID"
+    BOXCARS116K = "BoxCars116k"
+    COMP_CARS= "CompCars"
+    VERI = "Veri"
+    COMBINED = "Combined"
